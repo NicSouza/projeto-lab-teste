170ff5aaf0f6070c8a35ab3142a7cfa7170b9775
Nicole Souza <nicolemadeleines@gmail.com>
2024-04-24 23:07:47 -0300

    Projeto para avaliação

---
+++ src/main/java/com/fatec/lab/projetolabteste/ProjetoLabTesteApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.lab.projetolabteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLabTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLabTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/lab/projetolabteste/ProjetoLabTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.lab.projetolabteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLabTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
